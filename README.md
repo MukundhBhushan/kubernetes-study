f3439d8346e2ad33b251dc9351b91ced5b6faae2
MukundhBhushan <aknsmb@gmail.com>
2020-04-28 19:14:20 +0530

    added new stuff

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
 kubernetes commands and notes
+Kubernetes Node VM created by minikube
+
+kube-proxy -> service NodePort -> pods
+because of the proxy the nodeport address is not directly accessble by the local machine
+
+A config.ml file (file which contains info such as pods congif, services config etc) allows u to change only the image name, image prop
+use deployments instead to create pods
+
+![clusnode](https://matthewpalmer.net/kubernetes-app-developer/articles/networking-overview.png)
+pods: group containers doing similar tasks or containers which need to run alongside each other
+cluster: group of pods
+nodes: group of clusters
+Services: Takes care of the networking 
+
+Deployment: Runs a set of identical servers 
+- Good for dev and productions
+- Create deploymnets which creates pods instead of creating pods directly
+- add the clusterip yaml file for networking
+
+types of "kind" used in config files:
+- statefulset
+- replicaController
+- pod: pods group containers doing similar tasks or containers which need to run alongside each other
+- service: setup networking in the cluster
+
+## types of services: kind="service"
+![](./full.png)
+- clusterIP: Allow in-cluster networking services. It allows multiple pods to communicate with eachother. Outside traffic cannot access these ports directly. 
+- NodePort: exposes to specified port to the outside world (Only for dev stage)
+- LoadBalancer: (legacy ingres is the new way)
+- ingress: production stage better type of load balencer
+
+when listing deployments with kubectl get deployments
+Desired: the number of pod whech are running the latest versionof the <deployment.yml>file
+Current: The total number of pods running including the old and desired it can be greater than the "desired" coloumn value
+
+The ingress being used ingress-nginx
+
+## Ports in a service.yml file
+![](./port.png)
+
+- port: <Port> #port which another pod or container can access; other pods ---> current pod
+- targetPort: <Target Port> #containers port exposed as given in docker file. must be equal to the containerPort in kind:"Pod" configfile, sends any incoming trafic to <TargetPort>; current pod ---> other pod or outside world
+- nodePort: <port number> #Access by enduser select port between 3000-32767 if not specified it will be randomly generated used only in dev
+*nodeport not needed if using clusterip*
+
+## Volumes 
+
+### Volumes are of 3 types:
+- Kubernetes volume
+- persistant Volume
+- Persistant volume claim
+
+* Use volumes to protect db data from sudden loss in data or crashes
+* Volumes can help kubernetes to startup a new pod with the existing data when the old container is lost
+* if the replicas value is more than 1 then make sure each of the pod speak with eachother. Mulitple DB pods which dont talk to each other is a not recomended
+* Use something like persistant volume claim to setup volumes for DBs
+
+### Volumes are apart of the pod
+- if a container in a pod dies in a new container can be created and share the data from the previous volume.
+- if the pod dies then data is lost
+
+### Persistant volume vs Kubernetes volume
+![](./volume.png)
+- persistant volumes are not tied with any pods unlike Kubernetes volumes
+- therefore if the pod crashes the new pod can share data from the old persistant volume created unlike the Kubernetes volumes
+
+### Difference between Persisant volume claim persistante volume 
+- are like advertisments which show the types of storage options available/needs 
+- The amount of data being advertised can be either already present and ready to be used or be provisioned on the fly.
+- the amount of data store needed must be added to the config file 
+- kubernetes clusters has a couple of already provisioned data store options these are called staically provisioned persistant volumes. The cluster then check the congif file and wil provision the amount of storage option 
+- if the data storage amount specified was not statically created then a the asked amount is created on the fly and provisioned this is called Dynamically provisioned
+
+Acess mode:
+Types: 
+- ReadWriteOnce: Can be used by a single pod at a type
+- ReadOnlyMany: Multiple nodes can read many times
+- ReadWriteMany: Multiple nodes can read and write many times
+
+### Steps to Ingress:
+- Ingress config
+- Ingress controller: impliments the ingress with the help of the coonfig file
+- Accepts traffic and sends it to the appropriate service
+
+- HEML is used to administor 3rd party software in our kubernetes cluster. Used  - - - - install additional services 
+- tiller is the serverused to serve the application
+- heml cli - client
+- tiller - server
+- helm files
+    - Chart.yml - stores metadata about application. and version
+    - values.yml - stores default configuration
+    - requriments.yml - stores the dependencies
+    - charts/ (dir)- pakages are stores
+    - templates/ (dir)- where source templates are stores and are fed into the heml - templating engine 
+    - _helpers - stores functions 
--- kubernetes.bash
+++ kubernetes.bash
@@ -1,8 +1,11 @@
-#according to
+#according to 
 
 
 #Minikube is a tool that makes it easy to run Kubernetes locally.
 #Minikube runs a single-node Kubernetes cluster inside a VM on your laptop for users looking to try out Kubernetes or develop with it day-to-day.
+#kubernetes pod:- group of containers deployed together on the same host
+#pods group containers doing similar tasks or containers which need to run alongside each other
+#kubectl is used the manage what the node (or set of containers) are doing
 
 #to check if kubernetes installed
     minikube version
@@ -10,6 +13,38 @@
 #starting a cluster
     minikube start
 
+#to obtain IP address to access pods on local machine
+    minikube ip
+    
+#to use view deployment in browser after deployment is created
+    minikube service <cluster alias > --url
+
+#to enable dashboard
+    minikube addons enable dashboard
+#to view dashboard of all the kubernetes clusters
+    minikube dashboard
+#watch pod dashboard within system
+kubectl get pods -n kubernetes-dashboard -w 
+
+#to connect to the docker containers in cluster
+     eval $(minikube docker-env)
+    #this configures only the crrent working terminal window
+
+
+#kubernetes uses lable selectors to link pods together
+#lables are key value pairs <lable name>:<name>
+#in yml if
+
+metadata:
+    selectors:
+        lable: 
+            <lable name>:<name>
+
+#in yml file useing it
+    selector:
+        <lable name>: <name> 
+
+#Note: start minikube before executing any command
 
 #kubernetes-CLI
     kubectl
@@ -17,15 +52,111 @@
 #to check the health of the clusters
     kubectl cluster-info   
 
-#to view the nodes in the cluster
-    kubectl get nodes
+#feed a config file to kubectl
+kubectl apply -f <path to config file name>
+#apply: helps in changing the current config to our own
+
+#feeding a group o config files
+    kubectl apply -f <folder name>
+
+#to view the nodes/deployments/pods in the cluster
+    kubectl get nodes|deployments|pods|service
+
+#to delete node/deployment/pods
+    kubectl delete node|pod|deployment <alias>
+
+#to delete services
+    kubectl delete service <alias>
+
+#to delete a object 
+    kubectl delete -f <config file>
+
+#to get persistant volumes
+    kubectl get pv
+
+#to get all the volume claims
+    kubectl get pvc
+
+#to delete everything including cache docker images 
+    docker system prune -a 
+    #run command after configuring docoker to minikube 
 
 #deploying containers into kubernetes
     kubectl run <cluster alias > --image=<docker image name> --port=<port>
 
-#status of the pods
-kubectl get pods
+#to use pods a service has to be created
+    kubectl expose deployment <cluster alias > --type=<type> --port = <port no>
+    #type parameter
+    #    LoadBalancer
+    #     NodePort
+
+#to create secret to assign passwords keys etc
+    kubctl create secret <type of secret> <secret name | alias> --from-literal <key name> = <password value>
+
+    # type of secret: 
+    #     genric: most used
+    #     tls: http certificate 
+    #     docker-registry: custom private docker images
+# this is being done on local machine needs to be done again when in production
+
+#to get secret
+    kubctl get secret
 
 #providing dynamic ports to containers
     kubectl expose development <cluster alias > -p=<port> --type = NodePort #Nodeport allows for dynamic port allocation
-    
+
+#get details about a specific object 
+kubectl describe <object type> <object name> #gives info about <object name>
+kubectl describe <object type> #gives info about all the <object names> present in <object type>
+#eg: kubectl describe pod podName1
+
+#Imperative command to update deployment when image is updated
+#the version number must be specified in the docker image 
+
+kubectl set image <kind type> / <object name> <container name> = <image>:<new version number>
+#kind:
+    #-pod
+    #-deployment
+    #-services
+#container name:
+    #found in template > spec > container > name
+
+#eg kubectl set image deployment/client-deployment client= stephengrider/multi-client:v3
+
+#logs of a certain pod
+    kubectl logs <pods name>
+#connecting to shell of a certain pods
+    kubectl exec -it <pod name>
+
+# HEML is used to administor 3rd party software in our kubernetes cluster. Used install additional services 
+# tiller is the serverused to serve the application
+# heml cli - client
+# tiller - server
+# helm files
+#     Chart.yml - stores metadata about application. and version
+#     values.yml - stores default configuration
+#     requriments.yml - stores the dependencies
+#     charts/ (dir)- pakages are stores
+#     templates/ (dir)- where source templates are stores and are fed into the heml templating engine 
+#     _helpers - stores functions 
+
+
+#creating cluster service account
+    kubectl create serviceaccount --namespace kube-system tiller
+#creating service account cluster binding
+    kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
+#init heml
+    helm init --service-account tiller --upgrade
+#installing nginx when rbac is created
+    helm install stable/nginx-ingress --name my-nginx --set rbac.create=true
+#Creating the helm charts
+    helm create <folder name>
+    # the command creates an folder named <folder name>
+    #Charts/ 
+    #templates/_helper.tpl
+    #chart.yml
+    #values.yml
+    #delete the rest
+
+#to add dependencies add a requirements.yml file in the root of <folder name>
